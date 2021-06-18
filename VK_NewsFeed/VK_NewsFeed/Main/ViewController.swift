59db74020ff47f0b1d1a750474b2173110d608a0
Dzmitry <dima.semnovich.01@ickoud.com>
2021-06-18 23:06:43 +0300

    feat: initialized the project and implemented the basic UI

---
+++ VK_NewsFeed/VK_NewsFeed/Main/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK_NewsFeed
+//
+//  Created by Dzmitry Semenovich on 18.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VK_NewsFeed/VK_NewsFeed/Tabbar/TabbarController.swift
@@ -0,0 +1,17 @@
+//
+//  TabbarControllerViewController.swift
+//  VK_NewsFeed
+//
+//  Created by Dzmitry Semenovich on 18.06.21.
+//
+
+import UIKit
+
+class TabbarControllerViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
